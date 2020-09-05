149d4b22ab7388969b7639d8b9a303058bc96468
Younext19 <youneshaddam9@gmail.com>
2020-09-05 08:32:24 +0100

    RPS Done

--- static/js/script.js
+++ static/js/script.js
@@ -24,5 +24,73 @@ function GenerateCat(){
 
 // Challenge 3 : Rock Paper Scissors
 function rpsGame(yourChoice){
-    console.log(yourChoice);
+    console.log(yourChoice.id);
+    var humanChoice, botChoice;
+    var results;
+    var finalmsg;
+    humanChoice = yourChoice.id;
+    botChoice = randToRpsInt();
+    botChoice = numberToChoice(botChoice);
+    console.log(botChoice);
+    results = decideWinner(humanChoice, botChoice);
+    console.log(results);
+    var a=results[0];
+    var b= results[1];
+    if (a>b){
+        finalmsg="human won";
+    }
+    if(a<b){
+        finalmsg="computer won";
+    } 
+    if(a===b){
+        finalmsg="draw";
+    }
+    /*message = finalMessage(results);*/
+    rpsFrontEnd(humanChoice,botChoice,finalmsg);
+}
+function randToRpsInt(){
+    return Math.floor(Math.random() *3);
+}
+function numberToChoice(number){
+    return ['rock','paper','scissors'][number];
+}
+function decideWinner(humanChoice, botChoice){
+    var rpsDatabase ={
+        'rock':{'scissors':1,'rock':0.5,'paper':0},
+        'paper':{'rock':1, 'paper':0.5, 'scissors':0},
+        'scissors':{'paper':1, 'scissors':0.5,'rock':0}
+    }
+    var yourScore = rpsDatabase[humanChoice][botChoice];
+    var botScore = rpsDatabase[botChoice][humanChoice];
+    return [yourScore,botScore];
+}
+function rpsFrontEnd(humanimg,botimg,finalmsg){
+    var imageDb={
+        'rock':document.getElementById("rock").src,
+        'paper':document.getElementById("paper").src,
+
+        'scissors':document.getElementById("scissors").src
+    }
+    document.getElementById('rock').remove();
+    document.getElementById('paper').remove();
+    document.getElementById('scissors').remove();
+
+    var humanDiv=document.createElement('div');
+    var botDiv = document.createElement('div');
+    var messageDiv = document.createElement('div');
+
+    humanDiv.innerHTML="<img src='"+imageDb[humanimg]+"' height=150 width = 150 style='box-shadow : 0px 10px 50px rgba(37,50,233,1);'>" 
+    
+    messageDiv.innerHTML="<h1 "+finalmsg+"; font-size : 60px; padding:30px; '>"+finalmsg +"</h1>"
+
+    botDiv.innerHTML="<img src= '"+imageDb[botimg]+"'height=150 width = 150 style='box-shadow : 0px 10px 50px rgba(243,38,24,1);'>"
+
+    document.getElementById('flex-box-rps-div').appendChild(humanDiv);
+
+    document.getElementById('flex-box-rps-div').appendChild(messageDiv);
+
+    document.getElementById('flex-box-rps-div').appendChild(botDiv);
+
+
+
 }
